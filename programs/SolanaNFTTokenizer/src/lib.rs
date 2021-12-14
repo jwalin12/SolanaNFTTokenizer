18cb6b49e4c37463099438e5ceccd9dfc515bd8e
jwalin12 <jwalin12@berkeley.edu>
2021-12-13 21:58:02 -0800

    added some authority functionality

--- programs/SolanaNFTTokenizer/src/lib.rs
+++ programs/SolanaNFTTokenizer/src/lib.rs
@@ -8,7 +8,13 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod solana_nft_tokenizer {
     use super::*;
 
-    pub fn initialize_vault(ctx: Context<InitializeVault>) -> ProgramResult {
+    pub fn initialize_vault(ctx: Context<InitializeVault>, owner: PubKey) -> ProgramResult {
+        let vault_account = &mut ctx.accounts.vault_account;
+        vault_account.mint_fee = ctx.mint_fee;
+        vault_account.owner = owner;
+        vault_account.NFT_mint = ctx.NFT_mint;
+        vault_account.vault_mint = ctx.vault_mint.to_account_info;
+        vault_account.
         Ok(())
     }
 
@@ -19,15 +25,20 @@ pub mod solana_nft_tokenizer {
 
 
     pub fn exchange_tokens_for_NFT(ctx: Context<ExchangeTokensForNFT>) -> ProgramResult {
+        Ok(())
 
+    }
+
+    pub fn collect_fees(ctx: Context<CollectFees>) -> ProgramResult {
+        Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct InitializeVault {
-    #[account(init, payer = creator, has_one = )]
-    pub vaultAccount: Account<'info, VaultAccount>
-    pub mintFee: uint64,
+    #[account(init, payer = creator)]
+    pub vault_account: Account<'info, VaultAccount>
+    pub mint_fee: uint64,
     pub creator: Signer<'info>,
     #[account(init,
         token::mint = ownership_token_mint,
@@ -35,8 +46,8 @@ pub struct InitializeVault {
         seeds = [format!("{}{}{}{:?}", pool.token0Pubkey, pool.token1Pubkey, model.data, creator.signer_key()), (*"pool_tokenmint").to_string()],
         bump = pool_token_decimals,
         payer = creator)]
-    pub vaultMint: Box<Account<'info, TokenAccount>>,
-    pub NFTMint: AccountInfo<'info>,
+    pub vault_mint: Box<Account<'info, TokenAccount>>,
+    pub NFT_mint: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
 
 }
@@ -44,27 +55,24 @@ pub struct InitializeVault {
 
 #[account]
 pub struct VaultAccount {
-    pub mintFee: uint64,
-    pub owner: uint64,
-    pub vaultMint: AccountInfo<'info>,
-    pub NFTMint: AccountInfo<'info>,
+    pub mint_fee: uint64,
+    pub owner: PubKey,
+    pub vault_mint: AccountInfo<'info>,
+    pub NFT_mint: AccountInfo<'info>,
 
 }
 
 #[derive(Accounts)]
 pub struct MintSPLTokens {
-    pub mintFee: uint64,
     pub user: Signer<'info>,
     pub vault: AccountInfo<'info>,
-    pub NFTMint: AccountInfo<'info>,
+    pub NFT_mint: AccountInfo<'info>,
     
 }
 
 #[derive(Accounts)]
-pub struct MintSPLTokens {
-    pub mintFee: uint64,
-    pub user: Signer<'info>,
-    pub vault: AccountInfo<'info>,
-    pub NFTMint: AccountInfo<'info>,
-    
+pub struct CollectFees {
+    #[account(mut, has_one = authority)]
+    pub vault: Account<'info, VaultAccount>,
+    pub owner: Signer<'info>
 }
